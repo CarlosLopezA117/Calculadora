e1ee07541e60e1aaeb35de403ffa77ad2fd29125
Charly117 <espartan329@gmail.com>
2021-04-14 13:30:19 -0500

    1.4 Modificacion de valores int a double

--- src/App.java
+++ src/App.java
@@ -3,13 +3,13 @@ public class App {
 	public static void main(String[] args) {
 		
 	}
-	public static int suma (int a, int b) {
+	public static double suma (double a, double b) {
 		return a+b;
 	}
-	public static int resta (int a, int b) {
+	public static double resta (double a, double b) {
 		return a-b;
 	}
-	public static int multiplicar (int a, int b) {
+	public static double multiplicar (double a, double b) {
 		return a*b;
 	}
 }
